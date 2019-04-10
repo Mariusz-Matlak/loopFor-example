958d06117957ae7300596601c4e8785c012a8d7d
Mariusz Matlak <1mariusz.matlak@gmail.com>
2019-04-10 15:44:07 +0200

    add for loop and fixed bug

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const btn = document.querySelector('button');
-const liItems = document.querySelector('li');
+const liItems = document.querySelectorAll('li');
 
 let fontSize = 12;
 
+//for 
 btn.addEventListener('click', () => {
-    liItems.forEach((liItem) => {
-        liItem.style.display = "block";
-        liItem.style.fontSize = fontSize + "px";
-    })
     fontSize++;
+    for (let i = 0; i < liItems.length; i++) {
+        if (!liItems[i].style.display) {
+            liItems[i].style.display = "block";
+        }
+        liItems[i].style.display = "block";
+        liItems[i].style.fontSize = `${fontSize}px`;
+    }
 })
+
+// foreach
+// btn.addEventListener('click', () => {
+//     liItems.forEach((liItem) => {
+//         liItem.style.display = "block";
+//         liItem.style.fontSize = fontSize + "px";
+//     })
+//     fontSize++;
+// })
